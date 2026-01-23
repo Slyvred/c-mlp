698388d5825f3053dedb70f6d6c6449181583ba4
Slyvred <remihage2003@gmail.com>
2026-01-23 10:29:01 +0100

    mlp fonctionnel avec xor

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ void init_layer(layer *layer, int n_neurons, int n_parameters, function *activat
     layer->activation_function = activation_function;
     layer->n_neurons = n_neurons;
     layer->neurons = malloc(n_neurons * sizeof(neuron));
-    layer->outputs = malloc(n_neurons * sizeof(neuron));
+    layer->outputs = malloc(n_neurons * sizeof(double));
 
     for (int i = 0; i < n_neurons; i++) { init_neuron(&layer->neurons[i], n_parameters); }
 }
@@ -72,25 +72,22 @@ void init_layer(layer *layer, int n_neurons, int n_parameters, function *activat
 void forward(MLP *m, double* inputs, int n_inputs) {
     for (int i = 0; i < m->n_layers; i++) {
         layer* l = m->layers[i];
-        double* last_layer_outputs = l->outputs;
+        double* layer_inputs;
 
         // For input layer, input is the actual input
-        if (i == 0) last_layer_outputs = inputs;
+        if (i == 0) layer_inputs = inputs;
+        else layer_inputs = m->layers[i - 1]->outputs;
 
         for (int j = 0; j < l->n_neurons; j++) {
             neuron* n = &l->neurons[j];
-            n->output = l->activation_function->f(sum(last_layer_outputs, n->weights, n->bias, n->n_weights));
+            n->output = l->activation_function->f(sum(layer_inputs, n->weights, n->bias, n->n_weights));
             l->outputs[j] = n->output;
         }
-
-        // For the next layers, the input is the previous layer's outputs
-        last_layer_outputs = l->outputs;
     }
 }
 
 // Entraînement (Backpropagation)
-void train(MLP *m, double* target, double* raw_inputs, double lr) {
-
+void train(MLP *m, double* raw_inputs, double* target, double lr) {
     // Calculate output layer error
     layer* l = m->layers[m->n_layers - 1];
     for (int i = 0; i < l->n_neurons; i++) {
@@ -110,11 +107,10 @@ void train(MLP *m, double* target, double* raw_inputs, double lr) {
             neuron* n = &curr_layer->neurons[j];
             double error = 0;
 
-            // On somme les deltas de la couche suivante pondérés par les poids qui connectent à ce neurone
-            // Sum deltas of next layer weighted
+            // Sum deltas of next layer weighted by next neuron weights
             for (int k = 0; k < next_layer->n_neurons; k++) {
                 neuron* next_n = &next_layer->neurons[k];
-                // next_n->weights[j] est le poids connectant le neurone j actuel au neurone k suivant
+                // next_n->weights[j] is the weight connecting the current neuron with the next neuron k
                 error += next_n->delta * next_n->weights[j];
             }
             n->delta = error * m->layers[0]->activation_function->df(n->output);
@@ -147,22 +143,25 @@ void train(MLP *m, double* target, double* raw_inputs, double lr) {
 }
 
 int main(int argc, char** argv) {
-    srand(time(NULL)); // Pour la reproductibilité
+    srand(42); // Pour la reproductibilité
 
     layer hidden, output;
     function sig = {sigmoid, sigmoid_deriv};            // Activation function
-    init_layer(&hidden, 8, 1, &sig);                    // 8 neurons with 1 weights each
-    init_layer(&output, 4, hidden.n_neurons, &sig);     // 4 neurons with 8 weights each
+    init_layer(&hidden, 4, 2, &sig);                   // 8 neurons with 1 weights eachh
+    init_layer(&output, 1, hidden.n_neurons, &sig);     // 4 neurons with 8 weights each
 
     layer* layers[2] = {&hidden, &output};
     MLP model = {layers, 2};
 
-    double dataset[16][4] = {
-        {0,0,0,0}, {0,0,0,1}, {0,0,1,0}, {0,0,1,1},
-        {0,1,0,0}, {0,1,0,1}, {0,1,1,0}, {0,1,1,1},
-        {1,0,0,0}, {1,0,0,1}, {1,0,1,0}, {1,0,1,1},
-        {1,1,0,0}, {1,1,0,1}, {1,1,1,0}, {1,1,1,1}
-    };
+    double X[4][2] = {{0,0}, {0,1}, {1,0}, {1,1}};
+    double y[4][1] = {{0},   {1},   {1},   {0}};
+
+    // double dataset[16][4] = {
+    //     {0,0,0,0}, {0,0,0,1}, {0,0,1,0}, {0,0,1,1},
+    //     {0,1,0,0}, {0,1,0,1}, {0,1,1,0}, {0,1,1,1},
+    //     {1,0,0,0}, {1,0,0,1}, {1,0,1,0}, {1,0,1,1},
+    //     {1,1,0,0}, {1,1,0,1}, {1,1,1,0}, {1,1,1,1}
+    // };
 
     int epochs = 70000;
     double lr = 0.5;
@@ -175,20 +174,17 @@ int main(int argc, char** argv) {
 
     printf("Entraînement en cours...\n");
     for (int epoch = 0; epoch < epochs; epoch++) {
-        for (int i = 0; i < 16; i++) {
-            double input[1] = { i / 15.0 }; // Normalisation de l'entrée
-            forward(&model, input, 1);
-            double* outputs = model.layers[model.n_layers - 1]->outputs;
-            printf("In: %d | Out: [%.4f %.4f %.4f %.4f]\n", i, outputs[0], outputs[1], outputs[2], outputs[3]);
-            train(&model, dataset[i], input, lr);
-        }
+        int i = rand() % 4;
+        forward(&model, X[i], 1);
+        train(&model, X[i], y[i], lr);
     }
 
-    // printf("\n--- Résultats ---\n");
-    // for (int i = 0; i < 16; i++) {
-    //     forward(&m, (double)i, 15.0);
-    //     printf("In: %2d | Out: [%.0f %.0f %.0f %.0f]\n",
-    //            i, round(m.output[0]), round(m.output[1]), round(m.output[2]), round(m.output[3]));
-    // }
+    printf("\n--- Résultats ---\n");
+    for (int i = 0; i < 4; i++) {
+        forward(&model, X[i], 1);
+        double* outputs = model.layers[model.n_layers - 1]->outputs;
+        printf("In: %2d | Out: [%.2f] | Expected: [%f]\n",
+               i, outputs[0], y[i][0]);
+    }
     return 0;
 }
